fef1d0a47959cb2ff165a768e288cb42b2554a61
Rostyslav <rklyba@yahoo.com>
2021-09-14 16:14:05 +0300

    Did the Firebase setup

---
+++ src/firebase/config.js
@@ -0,0 +1,20 @@
+import * as firebase from 'firebase/app';
+import 'firebase/storage';
+import 'firebase/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBtWnrO3jeVnid7QMdy9GGoE995AdnpLoo",
+  authDomain: "photo-gallery-81903.firebaseapp.com",
+  projectId: "photo-gallery-81903",
+  storageBucket: "photo-gallery-81903.appspot.com",
+  messagingSenderId: "563942929699",
+  appId: "1:563942929699:web:1060014a5ebd91b510c896"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+
+export { projectStorage, projectFirestore };
